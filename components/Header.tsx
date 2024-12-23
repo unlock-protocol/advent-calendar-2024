966b84d2527f0896fd8e950081e18212921d10ce
Tomas Penaloza Aviles <tomatomic.dev@gmail.com>
2024-12-23 10:30:26 -0400

    Add linkWallet helper function when user is authenticated

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { toast } from "react-hot-toast";
 import { useAuth } from "../hooks/useAuth";
+import { usePrivy } from "@privy-io/react-auth";
 import Image from "next/image";
 
 function truncate(text = "", startChars = 5, endChars = 3, maxLength = 11) {
@@ -17,6 +18,15 @@ function truncate(text = "", startChars = 5, endChars = 3, maxLength = 11) {
 
 const Header = () => {
   const { login, logout, wallet } = useAuth();
+  const { authenticated, linkWallet } = usePrivy();
+  const handleLogin = async () => {
+    if (authenticated === true) {
+      linkWallet();
+    } else {
+      toast("Loading Privy to log you in...");
+      await login();
+    }
+  };
 
   return (
     <nav className='container relative'>
@@ -48,10 +58,7 @@ const Header = () => {
         ) : (
           <button
             className='cursor-pointer bg-cream text-primary font-bold px-1 md:p-2 rounded-full text-center uppercase'
-            onClick={() => {
-              toast("Loading Privy to log in you in...");
-              login();
-            }}
+            onClick={handleLogin}
           >
             Connect wallet
           </button>
