42d787764f735ddf7fc6b8289cab798fcb03100a
Tomas Penaloza Aviles <tomatomic.dev@gmail.com>
2024-12-24 17:21:38 -0400

    Fix text overflow

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,70 +1,15 @@
 import { usePrivy, useLogin } from "@privy-io/react-auth";
 import { useEffect, useMemo, useRef } from "react";
 import { useAccount } from "wagmi";
-import { toast } from "react-hot-toast";
 
 export function useAuth() {
-  const { linkEmail, user, logout } = usePrivy();
+  const { linkEmail, user, logout, authenticated, linkWallet } = usePrivy();
   const { address: wallet } = useAccount();
-  const errorShown = useRef(false);
-  const isMounted = useRef(false);
-  const toastId = useRef<string | undefined>();
   const { login: privyLogin } = useLogin({
     onError: (error) => {
-      if (isMounted.current && !errorShown.current && !toastId.current) {
-        toastId.current = toast.error("Failed to connect wallet");
-        console.error("Privy login error:", error);
-        errorShown.current = true;
-      }
+      console.error("Privy login error: ", error);
     },
   });
-
-  useEffect(() => {
-    isMounted.current = true;
-    return () => {
-      isMounted.current = false;
-      errorShown.current = false;
-      if (toastId.current) {
-        toast.dismiss(toastId.current);
-      }
-    };
-  }, []);
-  // useEffect(() => {
-  //   if (user) {
-  //     if (!user?.wallet?.address) {
-  //       // We have no wallet for that user. What can we do?
-  //       console.error("No wallet for user!");
-  //     } else {
-  //       // We have a wallet address. Let's ensure that the connected wallet matches it!
-  //       if (user?.wallet?.address !== wallet?.address) {
-  //         let found = false;
-  //         // If not, circle thru until we find the right one!
-  //         wallets.forEach((w) => {
-  //           if (w.address === user?.wallet?.address) {
-  //             found = true;
-  //             setActiveWallet(w);
-  //           }
-  //         });
-  //         if (!found) {
-  //           // console.error("No wallet matches the user's wallet!")
-  //           // We have a user that is connected but no matching wallet. What do we do?
-  //         }
-  //       } else {
-  //         // All good, user connected to the right wallet!
-  //       }
-  //     }
-  //   } else {
-  //     console.log({ wallet });
-  //     // No privy user. If we have a wallet, disconnect it
-  //     if (wallet) {
-  //       // Disconnect any wallet!
-  //       setActiveWallet(undefined);
-  //     } else {
-  //       // No wallet, no user. All good!
-  //     }
-  //   }
-  // }, [wallet, user, wallets]);
-
   useEffect(() => {
     if (user && !user?.email) {
       if (user?.google?.email) {
@@ -85,31 +30,13 @@ export function useAuth() {
     return true;
   }, [user]);
 
-  const login = async () => {
-    // if (user) {
-    //   if (user?.wallet?.address !== wallet?.address) {
-    //     let found = false;
-    //     // If not, circle thru until we find the right one!
-    //     wallets.forEach((w) => {
-    //       if (w.address === user?.wallet?.address) {
-    //         found = true;
-    //         setActiveWallet(w);
-    //       }
-    //     });
-    //     if (!found) {
-    //       if (user.wallet?.walletClientType !== "privy") {
-    //         await linkWallet();
-    //       } else {
-    //         // We will have to wait for user to log back in!
-    //         logout();
-    //       }
-    //     }
-    //   }
-    // } else {
-    privyLogin();
-    // }
+  const login = () => {
+    if (authenticated === true) {
+      linkWallet();
+    } else {
+      privyLogin();
+    }
   };
-
   return {
     linkEmail,
     user,
--- lib/days.ts
+++ lib/days.ts
@@ -230,7 +230,7 @@ Represented by a **Nintendo Entertainment System, calisthenics rings**, and a **
     role: "Unlock Labs Partnerships",
     description: `Valentina is a key member of the Unlock Labs Team, spearheading partnerships with a passion for demystifying crypto applications for newcomers. Her essence can be captured through three objects: a Kindle, running shoes, and bread, symbolizing her love for knowledge, fitness, and nurturing connections. 
     
-    Valentina's enthusiasm for the NFT space was ignited by her first significant acquisition, a World of Women NFT, which solidified her excitement to be part of this innovative community.`,
+Valentina's enthusiasm for the NFT space was ignited by her first significant acquisition, a World of Women NFT, which solidified her excitement to be part of this innovative community.`,
     link: "https://twitter.com/Valen_HL",
     image: "/images/nft/22.png",
   },
@@ -240,7 +240,7 @@ Represented by a **Nintendo Entertainment System, calisthenics rings**, and a **
     role: "Professional Karaoke Singer & Unlock Labs Team",
     description: `Christopher Carfi, aka Ccarfi, has been active in the crypto space since 2013 and champions onchain memberships and subscriptions as tools for creator independence within the Unlock Labs Team. He joined the Labs team in 2021 and witnessed the community flourish into thousands of passionate Locksmiths. 
     
-    Describing himself through “Dust, Fire, and Karaoke,” Ccarfi embodies creativity, resilience, and connection in the blockchain world and beyond. Additionally, he always has the DAO's back and tremendously supports everyone in the community!`,
+Describing himself through “Dust, Fire, and Karaoke,” Ccarfi embodies creativity, resilience, and connection in the blockchain world and beyond. Additionally, he always has the DAO's back and tremendously supports everyone in the community!`,
     link: "https://warpcast.com/ccarfi",
     image: "/images/nft/23.png",
   },
